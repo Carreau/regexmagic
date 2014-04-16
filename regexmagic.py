e89640a7bf2506a65364fa1b53771d0f199b3828
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-04-16 13:23:26 -0400

    Improve documentation

--- regexmagic.py
+++ regexmagic.py
@@ -1,18 +1,28 @@
-"""regexmagic provides a cell magic for the IPython Notebook called
-%%regex that runs regular expressions against lines of text without
+"""regexmagic provides several line and cell magics for the IPython
+Notebook, which run regular expressions against lines of text without
 the clutter of re.search(...) calls.  The output is colorized to show
 the span of each match.
 
-Usage:
+Available cell magics:
+    matchlines
+    imatchlines
 
-%%regex a+b
+Available line magics:
+    matchfile
+    imatchfile
+
+Example usage:
+
+%%matchlines a+b
 this text has no matches
 this line has one match: aaab
 about to match some more: aab
 
 Note: IPython presently interprets {x} to mean 'expand variable x', so
       regular expressions like '\d{4}' must be written as '\d{{4}}'.
-      We're working on it..."""
+      We're working on it...
+
+"""
 
 # This file is copyright 2013 by Matt Davis and Greg Wilson and
 # covered by the license at
@@ -22,8 +32,8 @@ import re
 from IPython.core.magic import Magics, magics_class, line_magic, cell_magic
 from IPython.display import display, HTML
 from IPython.html.widgets import interactive, fixed
-from IPython.core.error import UsageError
 
+# formatting templates
 PATTERN_TEMPL = '<span style="color:DarkGreen; font-weight:bold; font-style:italic;white-space: pre;">{0}</span><br/>'
 ERROR_TEMPL = '<span style="color:Red; font-weight:bold; font-style:italic;white-space: pre;">{0}</span><br/>'
 MATCH_TEMPL = '<span style="background:{0}; font-weight:bold;white-space: pre;">{1}</span>'
@@ -32,14 +42,24 @@ NOMATCH_TEMPL = '<span style="color:gray;white-space: pre;">{0}</span>'
 
 @magics_class
 class RegexMagic(Magics):
-    '''Provide the 'regex' calling point for the magic, and keep track of
-    alternating colors while matching.'''
+    '''Provide the calling points for the magic, and keep track of
+    alternating colors while matching.
+
+    '''
 
     Colors = ['Pink', 'Yellow']
 
     @line_magic
     def imatchfile(self, line, cell=None):
-        '''Read in a file, and match the regular expression to it.'''
+        '''Perform regular expression matching on the given file, using an
+        iteractively provided pattern.
+
+        Usage: %imatchfile <filename>
+
+        See the `%%imatchlines` cell magic for further documentation
+        and a description of available options.
+
+        '''
         filename = line.strip()
         with open(filename, 'r') as reader:
             text = reader.read()
@@ -47,7 +67,14 @@ class RegexMagic(Magics):
 
     @line_magic
     def matchfile(self, line, cell=None):
-        '''Read in a file, and match the given regular expression to it.'''
+        '''Perform regular expression matching on the given file using the
+        given pattern.
+
+        Usage: %matchfile <filename> <pattern>
+
+        See the `%%matchlines` cell magic for further documentation.
+
+        '''
         filename, pattern = line.split(' ', 1)
         filename = filename.strip()
         with open(filename, 'r') as reader:
@@ -56,13 +83,50 @@ class RegexMagic(Magics):
 
     @cell_magic
     def imatchlines(self, line, cell):
-        return interactive(self.handle_text, text=fixed(cell))
+        '''Perform regular expression matching on the contents of the cell,
+        using an interactively provided regular expression.
+
+        Usage: %%imatchlines
+
+        There are several options you can interactively specify:
+
+        ignore_case: Perform case-insensitive matching;
+        expressions like [A-Z] will match lowercase letters, too.
+
+        multiline: When specified, the pattern character '^' matches
+        at the beginning of the string and at the beginning of each
+        line (immediately following each newline); and the pattern
+        character '$' matches at the end of the string and at the end
+        of each line (immediately preceding each newline). By default,
+        '^' matches only at the beginning of the string, and '$' only
+        at the end of the string and immediately before the newline
+        (if any) at the end of the string.
+
+        dotall: Make the '.' special character match any character at
+        all, including a newline; without this flag, '.' will match
+        anything except a newline.
+
+        '''
+
+        return interactive(self.display_regex, text=fixed(cell))
 
     @cell_magic
     def matchlines(self, line, cell):
-        self.handle_text(pattern=line, text=cell)
+        '''Perform regular expression matching on the cell contents using the
+        given pattern.
 
-    def handle_text(self, pattern='', text='', ignore_case=False, multiline=False, dot_all=False):
+        Usage: %matchlines <pattern>
+
+        '''
+
+        self.display_regex(pattern=line, text=cell)
+
+    def display_regex(self, pattern='', text='',
+                      ignore_case=False, multiline=False, dot_all=False):
+        '''Compile the regular expression with the specified options, search
+        for matches, and format them appropriately.
+
+        '''
         # compile the regular expression, with flags
         flags = 0
         if ignore_case:
@@ -90,9 +154,13 @@ class RegexMagic(Magics):
         display(html_disp)
         return html_disp
 
-    def match(self, compiled_pattern, line):
+    def match(self, compiled_pattern, text):
+        '''Search for regular expression matches using the given compiled
+        pattern.
+
+        '''
         result = []
-        m = compiled_pattern.search(line)
+        m = compiled_pattern.search(text)
 
         while m:
             start = m.start()
@@ -102,19 +170,20 @@ class RegexMagic(Magics):
                 break
 
             # format all text up to the current match
-            result.append(NOMATCH_TEMPL.format(line[:start]))
+            result.append(NOMATCH_TEMPL.format(text[:start]))
             # format the current match
-            result.append(MATCH_TEMPL.format(self.this_color, line[start:end]))
+            result.append(MATCH_TEMPL.format(self.this_color, text[start:end]))
 
             # search for the next match
-            line = line[end:]
+            text = text[end:]
             self.this_color, self.next_color = self.next_color, self.this_color
-            m = compiled_pattern.search(line)
+            m = compiled_pattern.search(text)
 
-        if len(line) > 0:
-            result.append(NOMATCH_TEMPL.format(line))
+        if len(text) > 0:
+            result.append(NOMATCH_TEMPL.format(text))
 
         return ''.join(result)
+
 
 def load_ipython_extension(ipython):
     ipython.register_magics(RegexMagic)
